1cdcea139e322dc9eccd55141a7699f0e18e170a
ganeshpatil325.gp@gmail.com <ganeshpatil325.gp@gmail.com>
2020-01-09 10:57:35 +0530

    trying to merge automaatically

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,6 @@ import SignupForm from './SignupForm.js'
 import SignIn from './SignIn.js'
 import {Switch,Route} from 'react-router-dom'
 import Account from './Account'
-import government from './government'
 
 class Login extends Component {
 
@@ -15,7 +14,6 @@ class Login extends Component {
                     <Route exact path="/"component={SignIn}></Route>
                     <Route  path="/signup" component={SignupForm}></Route>
                     <Route  path="/account" component={Account}></Route>
-                    <Route  path="/government" component={government}></Route>
 
                 </Switch>
 
